23139b494b694a4f32c1277201bd844262716427
Jessica Carrillo <jessymamo@gmail.com>
2023-07-03 14:09:27 -0400

    Completed lab

---
+++ index.js
@@ -0,0 +1,39 @@
+/*
+The receivesAFunction function should:
+
+take a callback function as an argument
+call the callback function
+it doesn't matter what this function returns, so long as it calls the callback function
+
+The returnsANamedFunction function should:
+
+take no arguments
+return a named function
+
+The returnsAnAnonymousFunction function should:
+
+take no arguments
+return an anonymous function
+*/
+
+
+
+function receivesAFunction(callback) {
+    callback();
+}
+
+
+function dummyFunction() {
+    console.log("I'm a dummy function");
+}
+
+function returnsANamedFunction() {
+    return dummyFunction;
+}
+
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("I'm an anonymous function");
+    }
+}
